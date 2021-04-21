d96f82a64a3a184e2c8f18e9e7b9e0ddf70603d2
PMerdala <32525481+PMerdala@users.noreply.github.com>
2021-04-21 17:58:57 +0200

    init project

---
+++ src/main/java/pl/merdala/hexademo/HexaDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.merdala.hexademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/merdala/hexademo/HexaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.merdala.hexademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
